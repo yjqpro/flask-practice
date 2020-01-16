f671ff0a155b5a3799e9ecd3edc0406f31ee67d7
yjqpro <yjqpro@gmail.com>
2020-01-16 16:04:10 +0800

    Impl save user to db

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ from flask_login import (LoginManager, UserMixin, login_user, login_required)
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '12345678'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 
 class LoginForm(FlaskForm):
@@ -18,10 +30,17 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-class User(UserMixin):
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-    def __init__(self, id):
-        self._id = id
+
+class User(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
     def is_authenticated(self):
         return True
@@ -33,21 +52,15 @@ class User(UserMixin):
         return False
 
     def get_id(self):
-        return self._id
+        return self.id
 
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '12345678'
-
-
-login_manager = LoginManager(app)
+db.create_all()
 
 
 @login_manager.user_loader
 def load_user(user_id):
     print(user_id)
-    return User(id=user_id)
+    return User.query.filter_by(username=user_id).one_or_none()
 
 
 @login_manager.unauthorized_handler
@@ -72,6 +85,32 @@ def test():
     return 'test'
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        login_user(user)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+        flash('Register in successfully.')
+
+        next = request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        # if not is_safe_url(next):
+        # return abort(400)
+
+        return redirect(next or url_for('login'))
+    return render_template('register.html', form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # Here we use a class of some kind to represent and validate our
@@ -80,8 +119,11 @@ def login():
     form = LoginForm()
     print(form.validate_on_submit())
     if form.validate_on_submit():
-        user = User(form.username.data)
-        login_user(user)
+        user = User.query.filter_by(username=form.username.data).one_or_none()
+        if user:
+            login_user(user)
+        else:
+            return render_template('login.html', form=form)
 
         flash('Logged in successfully.')
 
